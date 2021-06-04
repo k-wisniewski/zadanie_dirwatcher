a5149262ab6bac9d4b0920f9937cdf7ed4dfc62d
Krzysztof Wiśniewski <krzysztof.wisniewski@codility.com>
2021-06-04 07:59:34 +0200

    Add tests and implementation of get_changes_since_last_checkpoint

--- dirwatcher/watcher_service.py
+++ dirwatcher/watcher_service.py
@@ -18,10 +18,10 @@ class NoPriorCheckpointSavedError(Exception):
 
 class WatcherService:
 
-    def __init__(self, dir_lister: Callable[[], list[Path]], store: CheckpointStore, hasher: Hasher):
-        self.dir_lister = dir_lister
-        self.store = store
-        self.hasher = hasher
+    def __init__(self, traverser: Callable[[], list[Path]], store: CheckpointStore, hasher: Hasher):
+        self._traverser = traverser
+        self._store = store
+        self._hasher = hasher
 
     def has_anything_changed(self) -> bool:
         """
@@ -34,7 +34,7 @@ class WatcherService:
         False - if there isn't
         """
         try:
-            checkpoints = self.store.load_checkpoints()
+            checkpoints = self._store.load_checkpoints()
         except FileNotFoundError as e:
             raise NoPriorCheckpointSavedError(e) from e
         else:
@@ -48,7 +48,7 @@ class WatcherService:
         :return:
         None
         """
-        self.store.save_checkpoints(self._hash_dir())
+        self._store.save_checkpoints(self._hash_dir())
 
     def get_changes_since_last_checkpoint(self) -> dict[Change, Iterator[Path]]:
         """
@@ -66,7 +66,21 @@ class WatcherService:
          - the list of paths affected by the change of type 2 is available under the key Change.NEW
          - the list of paths affected by the change of type 3 is available under the key Change.CONTENT_CHANGED
         """
-        raise NotImplementedError
+
+        try:
+            checkpoints = self._store.load_checkpoints()
+        except FileNotFoundError as e:
+            raise NoPriorCheckpointSavedError(e) from e
+        current_checkpoints = self._hash_dir()
+        diff = {
+            Change.DELETED: list(set(checkpoints.keys()) - set(current_checkpoints.keys())),
+            Change.NEW: list(set(current_checkpoints.keys()) - set(checkpoints.keys())),
+            Change.CONTENT_CHANGED: []
+        }
+        for current in set(current_checkpoints.keys()) & set(checkpoints.keys()):
+            if checkpoints[current] != current_checkpoints[current]:
+                diff[Change.CONTENT_CHANGED].append(current)
+        return diff
 
     def _hash_dir(self) -> dict[Path, str]:
-        return {item: self.hasher.hash_content(item) for item in self.dir_lister()}
+        return {item: self._hasher.hash_content(item) for item in self._traverser()}
--- dirwatcher/watcher_service_test.py
+++ dirwatcher/watcher_service_test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from dirwatcher.checkpoint_store_port import CheckpointStore
-from dirwatcher.watcher_service import WatcherService, NoPriorCheckpointSavedError
+from dirwatcher.watcher_service import WatcherService, NoPriorCheckpointSavedError, Change
 
 
 class _FakeCheckpointStoreAdapter(CheckpointStore):
@@ -82,3 +82,63 @@ def test_checkpoint_current_state_does_nothing_if_there_are_no_files_to_watch():
     service_under_test = WatcherService(lambda: [], store, _FakeHasher())
     service_under_test.checkpoint_current_state()
     assert store.saved_checkpoints == {}
+
+
+def test_get_changes_since_last_checkpoint_should_raise_if_no_prior_checkpoint_found():
+    service_under_test = WatcherService(
+        lambda: [Path('file1.txt'), Path('file2.txt')],
+        _FakeCheckpointStoreAdapter({}, True),
+        _FakeHasher()
+    )
+    with pytest.raises(NoPriorCheckpointSavedError):
+        service_under_test.get_changes_since_last_checkpoint()
+
+
+def test_get_changes_since_last_checkpoint_should_notice_new_files():
+    service_under_test = WatcherService(
+        lambda: [Path('file1.txt'), Path('file2.txt')],
+        _FakeCheckpointStoreAdapter({
+            Path("file1.txt"): "64496aedaadf981a8bd77f4ebb6e949eecaa15fb93cc3fa3fcb17acccd117e60",
+        }),
+        _FakeHasher()
+    )
+
+    result = service_under_test.get_changes_since_last_checkpoint()
+
+    assert result.get(Change.NEW) == [Path("file2.txt")]
+    assert result.get(Change.DELETED) == []
+    assert result.get(Change.CONTENT_CHANGED) == []
+
+
+def test_get_changes_since_last_checkpoint_should_notice_deleted_files():
+    service_under_test = WatcherService(
+        lambda: [Path('file1.txt')],
+        _FakeCheckpointStoreAdapter({
+            Path("file1.txt"): "64496aedaadf981a8bd77f4ebb6e949eecaa15fb93cc3fa3fcb17acccd117e60",
+            Path("file2.txt"): "bf470f3fe05eef6ba064ed3f9859aeddfeece239f9234f35448c95e943015b52",
+        }),
+        _FakeHasher()
+    )
+
+    result = service_under_test.get_changes_since_last_checkpoint()
+
+    assert result.get(Change.NEW) == []
+    assert result.get(Change.DELETED) == [Path("file2.txt")]
+    assert result.get(Change.CONTENT_CHANGED) == []
+
+
+def test_get_changes_since_last_checkpoint_should_notice_changed_files():
+    service_under_test = WatcherService(
+        lambda: [Path("file1.txt"), Path("file2.txt")],
+        _FakeCheckpointStoreAdapter({
+            Path("file1.txt"): "64496aedaadf981a8bd77f4ebb6e949eecaa15fb93cc3fa3fcb17acccd117e60",
+            Path("file2.txt"): "7b4dbecac0c118e9d79fd47832430bc80309866805c5517f97b3352218e8a0c4",
+        }),
+        _FakeHasher()
+    )
+
+    result = service_under_test.get_changes_since_last_checkpoint()
+
+    assert result.get(Change.NEW) == []
+    assert result.get(Change.DELETED) == []
+    assert result.get(Change.CONTENT_CHANGED) == [Path("file2.txt")]
