964d790921b81edaf83bc25281d84f81d75d44f0
Krzysztof Wiśniewski <krzysztof.wisniewski@codility.com>
2021-06-05 22:43:40 +0200

    Add implementation of traverser

--- dirwatcher/infrastructure/traverser.py
+++ dirwatcher/infrastructure/traverser.py
@@ -3,4 +3,7 @@ from typing import Callable, Iterator
 
 
 def make_traverser(path: Path) -> Callable[[], Iterator[Path]]:
-    pass
+    def list_dir():
+        yield from (item for item in path.iterdir() if item.is_file())
+
+    return list_dir
