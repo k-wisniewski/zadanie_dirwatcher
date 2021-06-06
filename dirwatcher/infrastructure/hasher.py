f730dd03d7752b6749c827b7379fd127ba4c5c60
Krzysztof Wiśniewski <krzysztof.wisniewski@codility.com>
2021-06-06 17:42:17 +0200

    Add hasher implementation

--- dirwatcher/infrastructure/hasher.py
+++ dirwatcher/infrastructure/hasher.py
@@ -1,7 +1,11 @@
+from hashlib import sha256
 from pathlib import Path
 
 
 class Hasher:
 
     def hash_content(self, path: Path) -> str:
-        ...
+        if not path.exists():
+            raise FileNotFoundError("Cannot hash non-existent file")
+        with open(path, "rb") as f:
+            return sha256(f.read()).hexdigest()
